97350403147295960f8a0fbd65de9e357ebda3b9
roctbb <roctbb@gmail.com>
2017-03-11 00:06:24 +0300

    add calendar

---
+++ seed/seedEvents.py
@@ -0,0 +1,106 @@
+import datetime
+
+from pymongo import MongoClient
+import random
+
+client = MongoClient('mongodb://roctbb:rcw000000@ds161029.mlab.com:61029/diabetlab')
+
+db = client['diabetlab']
+
+user = db['users'].find_one({'email': 'roctbb@gmail.com'})
+
+name = str(user['_id'])
+
+today = datetime.datetime.now().date()
+now_date = today + datetime.timedelta(days=-30)
+year = today+ datetime.timedelta(days=30*12)
+
+
+# seed insuline and ad
+
+events = []
+
+while now_date<year:
+
+    # suger
+    hours = [9, 12, 15, 18];
+    for hour in hours:
+        time = datetime.datetime.combine(now_date, datetime.time(hour, 0))
+        event = {
+            'time': time,
+            'type': 'GL',
+            'title': 'Измерение уровня глюкозы',
+            'user': name
+        }
+        events.append(event)
+
+    #bolus (food)
+    hours = [9,12,18];
+    for hour in hours:
+        time = datetime.datetime.combine(now_date, datetime.time(hour,30))
+        event = {
+            'time': time,
+            'type': 'FD',
+            'title': 'Прием пищи',
+            'description': '',
+            'user': name
+        }
+        events.append(event)
+
+    #basal
+    hours = [8, 20];
+    for hour in hours:
+        time = datetime.datetime.combine(now_date, datetime.time(hour, 0))
+        event = {
+            'time': time,
+            'type': 'ID',
+            'title': 'Базальный инсулин',
+            'description': '',
+            'user': name
+        }
+        events.append(event)
+
+    #ad
+    hours = [8, 20];
+    for hour in hours:
+        time = datetime.datetime.combine(now_date, datetime.time(hour, 40))
+        event = {
+            'time': time,
+            'type': 'AD',
+            'title': 'Измерение артериального давления',
+            'description': '',
+            'user': name
+        }
+        events.append(event)
+
+    now_date += datetime.timedelta(days=1)
+
+#seed disp
+now_date = today
+for i in range(2):
+    time = datetime.datetime.combine(now_date, datetime.time(12, 00))
+    event = {
+        'time': time,
+        'type': 'DS',
+        'title': 'Диспансеризация: общий анализ крови, биохимия, ЭКГ, осмотр ног, рентген грудной клетки, осмотр офтальмолога.',
+        'description': '',
+        'user': name
+    }
+    now_date += datetime.timedelta(days=30*12)
+    events.append(event)
+
+now_date = today
+for i in range(10):
+    time = datetime.datetime.combine(now_date, datetime.time(12, 00))
+    event = {
+        'time': time,
+        'type': 'GG',
+        'title': 'Анализ крови на гликированный гемоглобин.',
+        'description': '',
+        'user': name
+    }
+    now_date += datetime.timedelta(days = 30*3)
+    events.append(event)
+
+
+db['events'].insert(events)
--- server.py
+++ server.py
@@ -4,11 +4,14 @@ import uuid
 import time
 
 import sys
+
+import requests
 import tornado.ioloop
 import tornado.web
 import random
 import json
 
+from PIL import Image
 from bson import ObjectId
 from pymongo import MongoClient
 
@@ -40,6 +43,31 @@ class ExitHandler(BaseHandler):
     def get(self):
         self.clear_cookie('user');
         self.redirect('/login')
+
+class EventsHandler(BaseHandler):
+    def get(self):
+        colors = {'GL': '#7B2EC1', 'FD': '#7EBF28','ID': '#7CADD8','AD': '#116CC1','DS': '#C14C1B','GG': '#C14C1B'}
+        start = self.get_argument('start')
+        end = self.get_argument('end')
+        start_date = datetime.datetime.strptime(start, "%Y-%m-%d")
+        end_date = datetime.datetime.strptime(end, "%Y-%m-%d")
+        if not self.current_user:
+            return
+        name = tornado.escape.xhtml_escape(self.current_user)
+        events = db['events'].find({'user': name, 'time': {'$gte': start_date, '$lt': end_date}}).sort('time');
+        ready_events = []
+        for event in events:
+            ready_event = {
+                'title': event['title'],
+                'start': event['time'].strftime("%Y-%m-%dT%H:%M:%S"),
+                'end': (event['time']+datetime.timedelta(minutes=30)).strftime("%Y-%m-%dT%H:%M:%S"),
+                'allDay': False,
+                'backgroundColor': colors[event['type']],
+                'color': 'white'
+            }
+            ready_events.append(ready_event)
+        self.write(json.dumps(ready_events))
+
 
 class TherapyHandler(BaseHandler):
     def get(self):
@@ -73,7 +101,7 @@ class DiagnoHandler(BaseHandler):
         self.render('diagno.html', error='')
 
 
-class MainHandler(BaseHandler):
+class StatsHandler(BaseHandler):
     def get(self):
 
         if not self.current_user:
@@ -136,15 +164,117 @@ class MainHandler(BaseHandler):
                     ik_times=json.dumps(times['ik']), ik_values=json.dumps(values['ik']),
                     id_times=json.dumps(times['id']), id_values=json.dumps(values['id']), images=images, ctime=int(time.time()),
                     pills = pills, analisys=analysis, ad_times=ad_times, adl_values=adl_values, adh_values=adh_values,
-                    gh1 = user['GH1'],gh2 = user['GH2'], we_times=we_times, we_values=we_values, start_date = start.strftime("%Y-%m-%d %H:%M:%S"), end_date = end.strftime("%Y-%m-%d %H:%M:%S"))
+                    gh1 = user['GH1'],gh2 = user['GH2'], we_times=we_times, we_values=we_values, start_date = start.strftime("%Y-%m-%d %H:%M:%S"), end_date = end.strftime("%Y-%m-%d %H:%M:%S"),)
 
+def get_status_and_message_for_gl(value):
+    ok = True
+    text='Уровень глюкозы в крови в норме.'
+    if value > 5.5:
+        ok = False
+        text = 'Уровень глюкозы в крови выше нормы. Примите рекомендованную вам терапию, либо проконсультируйтесь со специалистом.'
+    elif value<4 and value>=3:
+        ok = False
+        text='Уровень глюкозы в крови ниже рекомендованного значение, вам необходимо его компенсировать.'
+    elif value < 3:
+        ok = False
+        text='ВАМ СРОЧНО НЕОБХОДИМО КОМПЕНСИРОВАТЬ УРОВЕНЬ ГЛЮКОЗЫ!'
+    return (ok, text)
+
+class MainHandler(BaseHandler):
+    def get(self):
+        if not self.current_user:
+            self.redirect("/login")
+            return
+        name = tornado.escape.xhtml_escape(self.current_user)
+        self.render('client.html', message='', error='', today=datetime.date.today().strftime('%Y-%m-%d'))
+    def post(self):
+        if not self.current_user:
+            self.redirect("/login")
+            return
+        name = tornado.escape.xhtml_escape(self.current_user)
+        type = self.get_argument('type')
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+        try:
+            if type=='IK':
+                value = str(self.get_argument('value')).replace(',','.')
+                medicine = self.get_argument('medicine')
+                data = {'type': 'IK', 'value':float(value), 'medicine':medicine, 'user_id': name, 'time': current_time}
+            if type=='ID':
+                value = str(self.get_argument('value')).replace(',','.')
+                medicine = self.get_argument('medicine')
+                data = {'type': 'ID', 'value':float(value), 'medicine':medicine, 'user_id': name, 'time': current_time}
+            if type=='MD':
+                value = str(self.get_argument('value')).replace(',','.')
+                medicine = self.get_argument('medicine')
+                data = {'type': 'MD', 'value':float(value), 'medicine':medicine, 'user_id': name, 'time': current_time}
+            if type=='GL':
+                value = str(self.get_argument('value')).replace(',','.')
+                data = {'type': 'GL', 'value':float(value), 'user_id': name, 'time': current_time}
+            if type=='FD':
+                fileinfo = self.request.files['photo'][0]
+                fname = fileinfo['filename']
+                extn = os.path.splitext(fname)[1].lower()
+                if extn!='.png' and extn!='.jpg' and extn!='.jpeg':
+                    raise Exception("error file type")
+                cname = str(uuid.uuid4()) + extn
+                fh = open(os.path.dirname(os.path.realpath(__file__))+'/images/' + cname, 'wb')
+                fh.write(fileinfo['body'])
+                im = Image.open(os.path.dirname(os.path.realpath(__file__))+'/images/'+cname);
+                im.thumbnail((300,300), Image.ANTIALIAS)
+                im.save(os.path.dirname(os.path.realpath(__file__))+'/images/'+cname)
+                food = ''
+                try:
+                    tvorog = requests.get('http://185.106.141.196:9991/roctbb?url=http://roctbb.net:5555/images/'+cname)
+                    food = tvorog.text
+                except:
+                    pass
+                data = {'type': 'FD', 'filename': cname, 'user_id': name, 'time': current_time, 'text': food}
+            if type=='GG':
+                value = str(self.get_argument('value')).replace(',', '.')
+                data = {'type': 'GG', 'value': float(value), 'user_id': name, 'time': current_time, 'name': 'Гликированный гемоглобин'}
+            if type == 'GG':
+                value = str(self.get_argument('value')).replace(',', '.')
+                data = {'type': 'WE', 'value': float(value), 'user_id': name, 'time': current_time,
+                        'name': 'Вес'}
+            if type=='LC':
+                ah_value = float(str(self.get_argument('ah_value')).replace(',', '.'))
+                lpn_value = float(str(self.get_argument('lpn_value')).replace(',', '.'))
+                lvn_value = float(str(self.get_argument('lvn_value')).replace(',', '.'))
+                tg_value = float(str(self.get_argument('tg_value')).replace(',', '.'))
+
+                data = {'type': 'AH', 'value': float(ah_value), 'user_id': name, 'time': current_time, 'name': 'Общий холестерин'}
+                db['results'].insert_one(data)
+                data = {'type': 'LPN', 'value': float(lpn_value), 'user_id': name, 'time': current_time, 'name': 'ЛПН'}
+                db['results'].insert_one(data)
+                data = {'type': 'LVN', 'value': float(lvn_value), 'user_id': name, 'time': current_time, 'name': 'ЛВН'}
+                db['results'].insert_one(data)
+                data = {'type': 'TG', 'value': float(tg_value), 'user_id': name, 'time': current_time, 'name': 'Триглицериды'}
+            if type=='AD':
+                lvalue = str(self.get_argument('lvalue')).replace(',', '.')
+                hvalue = str(self.get_argument('hvalue')).replace(',', '.')
+                data = {'type': 'AD', 'lvalue': float(lvalue),'hvalue': float(hvalue),'user_id': name, 'time': current_time}
+
+            db['results'].insert_one(data)
+
+            message = 'Данные сохранены! '
+            is_ok = True
+            if type=='GL': # если юзер заполнил глюкозу
+                value = str(self.get_argument('value')).replace(',','.')
+                is_ok, status_message = get_status_and_message_for_gl(float(value))
+                message += status_message
+            self.render('client.html', message_ok=is_ok, message=message, error='')
+        except Exception as e:
+            print(e)
+            self.render('client.html', message='', error='Проверьте правильность данных!')
 
 app = tornado.web.Application([
     (r"/exit", ExitHandler),
     (r"/", MainHandler),
+    (r"/stats", StatsHandler),
     (r"/therapy", TherapyHandler),
     (r"/diagno", DiagnoHandler),
     (r"/login", LoginHandler),
+    (r"/events", EventsHandler),
 
     ('/images/(.*)', tornado.web.StaticFileHandler, {'path': 'client/images'}),
 ], cookie_secret="ajidfjijIJIJDIFjmkdmfkm2348fhjn", debug=True)
